a89f3c36dd240b40542dfdd9b4d47e3dad7c2ac6
Aqua Dolf <aaqddf@icloud.com>
2024-02-09 15:46:36 +1100

    fix: pfp aspect ratio biz

--- app/api/image/holder/route.tsx
+++ app/api/image/holder/route.tsx
@@ -1,31 +1,33 @@
-import { ImageResponse } from '@vercel/og'
-import { NextRequest } from 'next/server'
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
 
 export const runtime = "edge";
 
 export async function GET(req: NextRequest) {
-
     // Fonts
     const barcode = await fetch(
-        new URL('../../../../assets/LibreBarcode128Text-Regular.ttf', import.meta.url),
+        new URL(
+            "../../../../assets/LibreBarcode128Text-Regular.ttf",
+            import.meta.url
+        )
     ).then((res) => res.arrayBuffer());
     const roboto = await fetch(
-        new URL('../../../../assets/RobotoMono-Regular.ttf', import.meta.url),
+        new URL("../../../../assets/RobotoMono-Regular.ttf", import.meta.url)
     ).then((res) => res.arrayBuffer());
 
     // Query Params
-    const { searchParams } = req.nextUrl
+    const { searchParams } = req.nextUrl;
 
-    const count = searchParams.get('count')
-    const rank = searchParams.get('rank')
+    const count = searchParams.get("count");
+    const rank = searchParams.get("rank");
 
-    const to = searchParams.get('to')
-    const username = searchParams.get('username')
-    const pfp = searchParams.get('pfp')
-    const ens = searchParams.get('ens')
-    const tokenAddy = searchParams.get('tokenAddy')
+    const to = searchParams.get("to");
+    const username = searchParams.get("username");
+    const pfp = searchParams.get("pfp");
+    const ens = searchParams.get("ens");
+    const tokenAddy = searchParams.get("tokenAddy");
 
-    const secondaryTextOpacity = 0.35
+    const secondaryTextOpacity = 0.35;
 
     return new ImageResponse(
         (
@@ -40,11 +42,12 @@ export async function GET(req: NextRequest) {
                     fontWeight: 700,
                     fontSize: 60,
                     // padding: 16,
-                    background: "linear-gradient(to bottom right, #343E90, #210446)",
+                    background:
+                        "linear-gradient(to bottom right, #343E90, #210446)",
                     flexDirection: "column",
                     justifyContent: "space-between",
-                    fontFamily: 'roboto',
-                    opacity: 1
+                    fontFamily: "roboto",
+                    opacity: 1,
                     // textAlign: "center",
                 }}
             >
@@ -53,23 +56,23 @@ export async function GET(req: NextRequest) {
                     style={{
                         padding: 0,
                         margin: 0,
-                        width: '100%',
+                        width: "100%",
                         display: "flex",
                         height: "12%",
                         flexDirection: "row",
                         justifyContent: "space-between",
-                        alignItems: 'flex-start'
+                        alignItems: "flex-start",
                     }}
                 >
                     <div
                         id="route-name"
                         style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'center',
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            justifyContent: "center",
                             fontSize: 64,
-                            opacity: secondaryTextOpacity
+                            opacity: secondaryTextOpacity,
                         }}
                     >
                         Holder
@@ -78,63 +81,65 @@ export async function GET(req: NextRequest) {
                         tw="p-1"
                         style={{
                             fontSize: 50,
-                            opacity: secondaryTextOpacity
+                            opacity: secondaryTextOpacity,
                         }}
                     >
                         Top {rank}
                     </span>
-
                 </div>
 
                 <div
                     id="mid-section"
                     style={{
-                        height: '400',
-                        width: '100%',
-                        display: 'flex',
+                        height: "400",
+                        width: "100%",
+                        display: "flex",
                         fontSize: 42,
                         alignItems: "center",
                         justifyContent: "center",
                         // letterSpacing: '-.08em',
-                        gap: 48
+                        gap: 48,
                     }}
                 >
-                {pfp ? (
-                    <div
-                        id="fc-user"
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                        }}
-                    >
-                        <img
-                            id="pfp"
-                            alt="pfp"
-                            height="400"
-                            src={`${pfp}`}
+                    {pfp ? (
+                        <div
+                            id="fc-user"
                             style={{
-                                opacity: 1,
-                                borderRadius: '100%'
+                                display: "flex",
+                                flexDirection: "column",
                             }}
-                        />
-                        <span>{username}</span>
-                    </div>
-                ) : (
-                    <span
-                        tw="p-1"
-                        style={{
-                            fontSize: 50,
-                            width: '200',
-                            wordBreak: 'break-all',
-                            // opacity: secondaryTextOpacity
-                        }}
-                    >
-                        {to}
-                    </span>
-                )}
+                        >
+                            <img
+                                id="pfp"
+                                alt="pfp"
+                                height="400"
+                                width="400"
+                                src={`${pfp}`}
+                                style={{
+                                    opacity: 1,
+                                    borderRadius: "100%",
+                                    objectFit: "cover", // Crop the image to cover the specified dimensions
+                                    objectPosition: "center", // Center the cropped area within the image container
+                                }}
+                            />
+                            <span>{username}</span>
+                        </div>
+                    ) : (
+                        <span
+                            tw="p-1"
+                            style={{
+                                fontSize: 50,
+                                width: "200",
+                                wordBreak: "break-all",
+                                // opacity: secondaryTextOpacity
+                            }}
+                        >
+                            {to}
+                        </span>
+                    )}
                     <div
                         style={{
-                            fontSize: 100
+                            fontSize: 100,
                         }}
                     >
                         {count}
@@ -143,15 +148,15 @@ export async function GET(req: NextRequest) {
                 <div
                     id="bottom-bar"
                     style={{
-                        height: '15%',
-                        width: '100%',
-                        display: 'flex',
+                        height: "15%",
+                        width: "100%",
+                        display: "flex",
                         fontSize: 88,
                         alignItems: "flex-end",
                         justifyContent: "flex-start",
-                        letterSpacing: '-.08em',
+                        letterSpacing: "-.08em",
                         gap: 48,
-                        opacity: 0.3
+                        opacity: 0.3,
                     }}
                 >
                     <span
@@ -159,20 +164,18 @@ export async function GET(req: NextRequest) {
                         tw="p-2 m-0"
                         style={{
                             fontFamily: '"roboto"',
-
                         }}
                     >
-                        {'/' + tokenAddy}
+                        {"/" + tokenAddy}
                     </span>
                     <span
                         id="empty"
                         tw="p-2 m-0"
                         style={{
-                            width: '30px',
-                            height: '100%',
+                            width: "30px",
+                            height: "100%",
                         }}
-                    >
-                    </span>
+                    ></span>
                 </div>
             </div>
         ),
@@ -181,15 +184,15 @@ export async function GET(req: NextRequest) {
             height: 630,
             fonts: [
                 {
-                    name: 'barcode',
+                    name: "barcode",
                     data: barcode,
-                    style: 'normal',
+                    style: "normal",
                 },
                 {
-                    name: 'roboto',
+                    name: "roboto",
                     data: roboto,
-                    style: 'normal',
-                }
+                    style: "normal",
+                },
             ],
         }
     );
