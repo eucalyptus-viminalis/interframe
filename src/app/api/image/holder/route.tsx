416b7d599c3597e98a1eaccea62e72ad08866c66
Aqua Dolf <aaqddf@icloud.com>
2024-02-12 23:45:20 +1100

    Top -> Rank

--- src/app/api/image/holder/route.tsx
+++ src/app/api/image/holder/route.tsx
@@ -84,7 +84,7 @@ export async function GET(req: NextRequest) {
                             opacity: secondaryTextOpacity,
                         }}
                     >
-                        Top {rank}
+                        Rank {rank}
                     </span>
                 </div>
 
