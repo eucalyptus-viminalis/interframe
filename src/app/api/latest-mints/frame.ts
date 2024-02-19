940ad93362f98a50b1c6eaf18a7fde10e7d485f2
Aqua Dolf <aaqddf@icloud.com>
2024-02-19 19:56:02 +1100

    change routing

--- src/app/api/latest-mints/frame.ts
+++ src/app/api/latest-mints/frame.ts
@@ -17,84 +17,84 @@ const MAX_ROUTE_LENGTH = 10; // Maximum number of top holders to show
 const LAST_INDEX = MAX_ROUTE_LENGTH - 1; // Last page index to show
 
 export async function MintFrame(idx: number, collectionAddress: string) {
-    const frameContent: FrameContent = {
-        frameButtons: [],
-        frameImageUrl: AppConfig.hostUrl,
-        framePostUrl:
-            AppConfig.hostUrl +
-            `/api/latest-mints?idx=${idx}&tokenAddy=${collectionAddress}`,
-        frameTitle: "see Zora | latest mints",
-        frameVersion: "vNext",
-    };
-    // Get mint transfers
-    const data = await zdk.mints({
-        where: {
-            collectionAddresses: [collectionAddress],
+  const frameContent: FrameContent = {
+    frameButtons: [],
+    frameImageUrl: AppConfig.hostUrl,
+    framePostUrl:
+      AppConfig.hostUrl +
+      `/api/latest-mints?idx=${idx}&tokenAddy=${collectionAddress}`,
+    frameTitle: "see Zora | latest mints",
+    frameVersion: "vNext",
+  };
+  // Get mint transfers
+  const data = await zdk.mints({
+    where: {
+      collectionAddresses: [collectionAddress],
+    },
+    includeFullDetails: false,
+    includeMarkets: false,
+    sort: {
+      sortDirection: "DESC" as SortDirection,
+      sortKey: "TIME" as MintSortKey,
+    },
+    pagination: {
+      limit: 10,
+    },
+  });
+  // Set buttons
+  frameContent.frameButtons =
+    idx >= data.mints.nodes.length - 1 || idx >= LAST_INDEX
+      ? [
+        {
+          action: "post",
+          label: "<back",
         },
-        includeFullDetails: false,
-        includeMarkets: false,
-        sort: {
-            sortDirection: "DESC" as SortDirection,
-            sortKey: "TIME" as MintSortKey,
+        {
+          action: "post",
+          label: "<summary",
         },
-        pagination: {
-            limit: 10,
+      ]
+      : [
+        {
+          action: "post",
+          label: "<back",
         },
-    });
-    // Set buttons
-    frameContent.frameButtons =
-        idx >= data.mints.nodes.length - 1 || idx >= LAST_INDEX
-            ? [
-                  {
-                      action: "post",
-                      label: "<back",
-                  },
-                  {
-                      action: "post",
-                      label: "<home>",
-                  },
-              ]
-            : [
-                  {
-                      action: "post",
-                      label: "<back",
-                  },
-                  {
-                      action: "post",
-                      label: "next>",
-                  },
-                  {
-                      action: "post",
-                      label: "<home>",
-                  },
-              ];
-    // If it's the last holder to show, append a query param to postUrl
-    if (idx >= data.mints.nodes.length - 1 || idx >= LAST_INDEX) {
-        frameContent.framePostUrl += "&last=true";
-    }
-    const mint = data.mints.nodes[idx];
-    console.log(`mint: ${JSON.stringify(mint, null, 2)}`)
-    const img = mint.token?.image?.url
-    // const img = sanitiseForPossibleIPFS(mint.token?.image?.url)
-    const tokenId = mint.mint.tokenId;
-    let mintTimestamp = mint.mint.transactionInfo.blockTimestamp;
-    mintTimestamp = encodeURIComponent(timeAgo.format(new Date(mintTimestamp)));
-    const to = mint.mint.toAddress;
-    const tokenName = mint.token?.tokenContract?.name
-    frameContent.frameImageUrl += `/api/image/mint?img=${img ? ipfsSrcToUrl(img) : ''}&tokenId=${tokenId}&mintTimestamp=${mintTimestamp}&to=${to}`;
-    frameContent.frameImageUrl += `&tokenName=${tokenName ? encodeURIComponent(tokenName) : ''}`
+        {
+          action: "post",
+          label: "next>",
+        },
+        {
+          action: "post",
+          label: "<summary",
+        },
+      ];
+  // If it's the last holder to show, append a query param to postUrl
+  if (idx >= data.mints.nodes.length - 1 || idx >= LAST_INDEX) {
+    frameContent.framePostUrl += "&last=true";
+  }
+  const mint = data.mints.nodes[idx];
+  console.log(`mint: ${JSON.stringify(mint, null, 2)}`)
+  const img = mint.token?.image?.url
+  // const img = sanitiseForPossibleIPFS(mint.token?.image?.url)
+  const tokenId = mint.mint.tokenId;
+  let mintTimestamp = mint.mint.transactionInfo.blockTimestamp;
+  mintTimestamp = encodeURIComponent(timeAgo.format(new Date(mintTimestamp)));
+  const to = mint.mint.toAddress;
+  const tokenName = mint.token?.tokenContract?.name
+  frameContent.frameImageUrl += `/api/image/mint?img=${img ? ipfsSrcToUrl(img) : ''}&tokenId=${tokenId}&mintTimestamp=${mintTimestamp}&to=${to}`;
+  frameContent.frameImageUrl += `&tokenName=${tokenName ? encodeURIComponent(tokenName) : ''}`
 
-    try {
-        // Example
-        // vitalik.eth
-        // const to = "0xd8da6bf26964af9d7eed9e03e53415d37aa96045";
-        const user = await client.lookupUserByVerification(to);
-        const username = user.result.user.username;
-        const pfp = user.result.user.pfp.url;
-        frameContent.frameImageUrl += `&username=${username}&pfp=${pfp}`;
-    } catch {
-        console.log("neynar lookupUserByVerification failed.");
-        // TODO: handle error
-    }
-    return Frame200Response(frameContent);
+  try {
+    // Example
+    // vitalik.eth
+    // const to = "0xd8da6bf26964af9d7eed9e03e53415d37aa96045";
+    const user = await client.lookupUserByVerification(to);
+    const username = user.result.user.username;
+    const pfp = user.result.user.pfp.url;
+    frameContent.frameImageUrl += `&username=${username}&pfp=${pfp}`;
+  } catch {
+    console.log("neynar lookupUserByVerification failed.");
+    // TODO: handle error
+  }
+  return Frame200Response(frameContent);
 }
--- src/app/api/latest-mints/route.ts
+++ src/app/api/latest-mints/route.ts
@@ -8,9 +8,9 @@ import { MintFrame } from "./frame";
 // idx?
 // tokenAddy
 export async function GET(req: NextRequest) {
-    const tokenAddy = req.nextUrl.searchParams.get("tokenAddy") as string;
-    const idx = req.nextUrl.searchParams.get("idx");
-    return await MintFrame(idx ? +idx : 0, tokenAddy);
+  const tokenAddy = req.nextUrl.searchParams.get("tokenAddy") as string;
+  const idx = req.nextUrl.searchParams.get("idx");
+  return await MintFrame(idx ? +idx : 0, tokenAddy);
 }
 
 // POST /api/latest-mints
@@ -18,41 +18,42 @@ export async function GET(req: NextRequest) {
 // idx
 // tokenAddy
 export async function POST(req: NextRequest) {
-    const searchParams = req.nextUrl.searchParams;
-    const idx = +searchParams.get("idx")!;
-    const tokenAddy = searchParams.get("tokenAddy") as string;
-    const last = searchParams.get("last");
-    const data: FrameSignaturePacket = await req.json();
+  const searchParams = req.nextUrl.searchParams;
+  const idx = +searchParams.get("idx")!;
+  const tokenAddy = searchParams.get("tokenAddy") as string;
+  const last = searchParams.get("last");
+  const data: FrameSignaturePacket = await req.json();
 
-    // Route request
-    if (data.untrustedData.buttonIndex == 1 && idx == 0) {
-        // Case 2: Pressed Back button from page index 0
-        const res = await fetch(
-            AppConfig.hostUrl + `/api/summary?tokenAddy=${tokenAddy}`
-        );
-        return new Response(res.body, {
-            headers: { "Content-Type": "text/html" },
-        });
-    } else if (data.untrustedData.buttonIndex == 1) {
-        // Case 3: Pressed Back button from page index not 0
-        return await MintFrame(idx - 1, tokenAddy);
-    } else if (data.untrustedData.buttonIndex == 2 && last != 'true') {
-        // Case 4: Pressed Next button
-        return await MintFrame(idx + 1, tokenAddy);
-    } else if (data.untrustedData.buttonIndex == 2 && last == 'true') {
-        // Case 4: Pressed "home" button
-        const res = await fetch(AppConfig.hostUrl + `/api/home`);
-        return new Response(res.body, {
-            headers: { "Content-Type": "text/html" },
-        });
-    } else if (data.untrustedData.buttonIndex == 3) {
-        // Case 5: pressed "home" button
-        const res = await fetch(AppConfig.hostUrl + `/api/home`);
-        return new Response(res.body, {
-            headers: { "Content-Type": "text/html" },
-        });
-    }
-    // Case 5: Pressed redirect to Zora button
-    console.log(`FIXME: routing case not found`);
-    return new Response();
+  // Route request
+  if (data.untrustedData.buttonIndex == 1 && idx == 0) {
+    // Case 2: Pressed Back button from page index 0
+    const res = await fetch(
+      AppConfig.hostUrl + `/api/summary?tokenAddy=${tokenAddy}`
+    );
+    return new Response(res.body, {
+      headers: { "Content-Type": "text/html" },
+    });
+  } else if (data.untrustedData.buttonIndex == 1) {
+    // Case 3: Pressed Back button from page index not 0
+    return await MintFrame(idx - 1, tokenAddy);
+  } else if (data.untrustedData.buttonIndex == 2 && last != 'true') {
+    // Case 4: Pressed Next button
+    return await MintFrame(idx + 1, tokenAddy);
+  } else if (data.untrustedData.buttonIndex == 2 && last == 'true') {
+    // Case 4: Pressed "summary" button
+    const res = await fetch(AppConfig.hostUrl + `/api/summary?tokenAddy=${tokenAddy}`);
+    return new Response(res.body, {
+      headers: { "Content-Type": "text/html" },
+    });
+  } else if (data.untrustedData.buttonIndex == 3) {
+    // Case 5: pressed "summary" button
+    const res = await fetch(AppConfig.hostUrl + `/api/summary?tokenAddy=${tokenAddy}`);
+    return new Response(res.body, {
+      headers: { "Content-Type": "text/html" },
+    });
+  } else {
+    // Bad route
+    const res = await fetch(AppConfig.hostUrl + `/api/error`)
+    return new Response(res.body, { headers: { 'content-type': 'text/html' } })
+  }
 }
